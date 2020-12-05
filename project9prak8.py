b3e1f530e9146c63e9d1f0da8909d56ce234464a
FarhanAryaFidiansyah <71634989+FarhanAryaFidiansyah@users.noreply.github.com>
2020-12-06 01:21:56 +0700

    Update project9prak8.py

--- project9prak8.py
+++ project9prak8.py
@@ -1,5 +1,6 @@
 buah = {"apel" : 5000, "jeruk" : 8500, "mangga" : 7800, "duku" :6500}
 nama = input("Nama buah yang ingin dibeli :")
-kg = int(input("Berapa Kg :"))
+kg = int(input("Berapa Kg                   :"))
 harga = buah[nama] * kg
-print("Harga yang perlu dibayarkan adalah", harga)
+print("-------------------------")
+print("Total harga                 :", harga)
