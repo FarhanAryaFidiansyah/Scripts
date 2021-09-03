3e3a8e08507abcd2ceec3af2000bdeaafc61e5ad
FarhanAryaFidiansyah <71634989+FarhanAryaFidiansyah@users.noreply.github.com>
2021-09-03 21:33:50 +0700

    Create php0202.php

---
+++ php0202.php
@@ -0,0 +1,13 @@
+<?php
+    $a = ['nama' => 'Sulis','nim' => 'K3510001','alamat' => 'Solo','bidang' => 'RPL'];
+    $b = ['nama' => 'Rendi','nim' => 'k1424567','alamat' => 'Klaten','bidang' => 'TKJ'];
+    $c = ['nama' => 'Jatmiko','nim' => 'k1424567','alamat' => 'Boyolali','bidang' => 'Multimedia'];
+    $d = ['nama' => 'Feri','nim' => 'k1424567','alamat' => 'Wonogiri','bidang' => 'RPL'];
+    print_r($a);
+    echo "<br>";
+    print_r($b);
+    echo "<br>";
+    print_r($c);
+    echo "<br>";
+    print_r($d);
+?>
